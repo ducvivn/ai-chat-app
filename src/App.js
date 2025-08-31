972066627c6656a5a54b64d403352f0260c819a1
Duc-Vi Nguyen <duc-vi97@hotmail.com>
2025-08-31 21:38:20 +0200

    update 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [meldinger, setMeldinger] = useState([]);
+  const [input, setInput] = useState("");
+  const bottomRef = useRef(null);
+
+  function sendMelding(e) {
+    e.preventDefault();
+    if (!input.trim()) return;
+
+    setMeldinger([...meldinger, { role: "bruker", content: input }]);
+    setInput("");
+  }
+
+  useEffect(() => {
+  bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [meldinger]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Boks">
+          <div className="Chat">
+            {meldinger.map((melding, index) => 
+              (<p key={index}> {melding.content} </p>))}
+            <div ref={bottomRef}></div> 
+          </div>
+          <div className= "Skrivefelt">
+            <input
+            value = {input}
+            onChange={(e) => setInput(e.target.value)} 
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+              sendMelding(e);
+            }}}
+            placeholder = "Spør meg hva som helst"></input>
+            <button onClick = {sendMelding} >Send</button>
+          </div>
+      </div>
     </div>
   );
 }
-
-export default App;
